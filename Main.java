dd3b58fc330acc939c26e2cf9f44700ba1654615
M. AsadUllah <68208088+m-asadullah@users.noreply.github.com>
2023-12-31 09:01:33 -0700

    commit

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        String[] strings = {"16", "32", "48", "96", "144", "192"};
-        for (String string : strings) {
-            System.out.println("<link rel=\"icon\" type=\"image/png\" sizes=\"" + string + "x" + string + "\" href=\"icons/favicon-" + string + "x" + string + ".png\">");
-        }
-    }
-}
