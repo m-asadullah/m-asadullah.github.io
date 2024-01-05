a0c21c4b00807c12119587a8b7420f4daa7e2464
M. AsadUllah <68208088+m-asadullah@users.noreply.github.com>
2024-01-05 05:02:01 -0700

    commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Source for the [`@m-asadullah`](https://www.google.com/search?q=github+m-asadullah) OSS Hub
+
 ### https://m-asadullah.github.io/ 
--- service-worker.js
+++ service-worker.js
@@ -2,27 +2,27 @@ const CACHE_NAME = `profile-v1`;
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    cache.addAll([
-      '/',
-      'scripts/script.js',
-      'styles/app.css'
-    ]);
-  })());
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll([
+            '/',
+            'assets/styles/app.css',
+            'assets/scripts/script.js'
+        ]);
+    })());
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).then(res => {
-      const resClone = res.clone();
-      caches.open(CACHE_NAME).then(cache => {
-        // Only cache requests that start with http
-        if (event.request.url.startsWith('http')) {
-          cache.put(event.request, resClone);
-        }
-      });
-      return res;
-    }).catch(err => caches.match(event.request).then(res => res))
-  );
+    event.respondWith(
+        fetch(event.request).then(res => {
+            const resClone = res.clone();
+            caches.open(CACHE_NAME).then(cache => {
+                // Only cache requests that start with http
+                if (event.request.url.startsWith('http')) {
+                    cache.put(event.request, resClone);
+                }
+            });
+            return res;
+        }).catch(err => caches.match(event.request).then(res => res))
+    );
 });
